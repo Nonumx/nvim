a709a3d8b5d6b057b1dffcac1ea5ffe307f2966c
nonumx <nonumx@gmail.com>
2023-06-28 22:25:46 +0800

    add basic options

---
+++ init.lua
@@ -0,0 +1,18 @@
+local opt = vim.opt
+
+-- Indenting
+opt.expandtab = true
+opt.smartindent = true
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.softtabstop = 4
+
+-- Mouse
+opt.mouse = "a"
+
+-- Search
+opt.ignorecase = true
+opt.smartcase = true
+
+-- True color
+opt.termguicolors = true
