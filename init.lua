c6711935566d8ef56ed60708ef1f521297e57d0e
nonumx <nonumx@gmail.com>
2023-06-29 17:40:37 +0800

    add lazy.nvim and neo-tree

--- init.lua
+++ init.lua
@@ -16,3 +16,38 @@ opt.smartcase = true
 
 -- True color
 opt.termguicolors = true
+
+-- Leader key
+vim.g.mapleader = " "
+
+-- lazy.nvim bootstrap
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local neo_tree = {
+    "nvim-neo-tree/neo-tree.nvim",
+    keys = {
+      { "<leader>e", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+    },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+    }
+}
+
+local plugins = {
+    neo_tree
+}
+
+require("lazy").setup(plugins)
