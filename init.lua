c5d21ba38e0eb2476a333869dcfe7cf7d2e22f64
nonumx <nonumx@gmail.com>
2023-06-29 23:00:12 +0800

    add nvim-cmp and formatter

--- init.lua
+++ init.lua
@@ -46,8 +46,119 @@ local neo_tree = {
     }
 }
 
+local LuaSnip = {
+    "L3MON4D3/LuaSnip",
+    version = "1.2.1",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+    end
+}
+
+local cmp = {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+        LuaSnip,
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+    },
+    config = function()
+        local cmp = require("cmp")
+        local luasnip = require("luasnip")
+        local has_words_before = function()
+            unpack = unpack or table.unpack
+            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+            return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+        end
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end
+            },
+            mapping = {
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                    elseif has_words_before() then
+                        cmp.complete()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" })
+            },
+            sources = cmp.config.sources({
+                { name = "nvim_lsp" },
+                { name = "luasnip" },
+                { name = "buffer" },
+            }),
+        })
+    end
+}
+
+local formatter = {
+    "mhartington/formatter.nvim",
+    config = function()
+        local util = require("formatter.util")
+
+        -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
+        require("formatter").setup({
+            -- Enable or disable logging
+            logging = true,
+            -- Set the log level
+            log_level = vim.log.levels.WARN,
+            -- All formatter configurations are opt-in
+            filetype = {
+                -- Formatter configurations for filetype "lua" go here
+                -- and will be executed in order
+                lua = {
+                    -- "formatter.filetypes.lua" defines default configurations for the
+                    -- "lua" filetype
+                    require("formatter.filetypes.lua").stylua,
+
+                    -- You can also define your own configuration
+                    function()
+                        -- Supports conditional formatting
+                        if util.get_current_buffer_file_name() == "special.lua" then
+                            return nil
+                        end
+
+                        -- Full specification of configurations is down below and in Vim help
+                        -- files
+                        return {
+                            exe = "stylua",
+                            args = {
+                                "--search-parent-directories",
+                                "--stdin-filepath",
+                                util.escape_path(util.get_current_buffer_file_path()),
+                                "--",
+                                "-",
+                            },
+                            stdin = true,
+                        }
+                    end
+                },
+
+                -- Use the special "*" filetype for defining formatter configurations on
+                -- any filetype
+                ["*"] = {
+                    -- "formatter.filetypes.any" defines default configurations for any
+                    -- filetype
+                    require("formatter.filetypes.any").remove_trailing_whitespace
+                }
+            }
+        })
+    end
+}
+
 local plugins = {
-    neo_tree
+    neo_tree,
+    cmp,
+    formatter
 }
 
 require("lazy").setup(plugins)
+
