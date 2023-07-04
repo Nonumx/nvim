e3efb15da490966b80aa84e0b389ba46540d9407
nonumx <nonumx@gmail.com>
2023-07-04 22:27:06 +0800

    add: cmp keymapping and icons

--- init.lua
+++ init.lua
@@ -100,11 +100,35 @@ local cmp = {
     config = function()
         local cmp = require("cmp")
         local luasnip = require("luasnip")
-        local has_words_before = function()
-            unpack = unpack or table.unpack
-            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-            return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-        end
+
+        local kind_icons = {
+            Text = "",
+            Method = "󰆧",
+            Function = "󰊕",
+            Constructor = "",
+            Field = "󰇽",
+            Variable = "󰂡",
+            Class = "󰠱",
+            Interface = "",
+            Module = "",
+            Property = "󰜢",
+            Unit = "",
+            Value = "󰎠",
+            Enum = "",
+            Keyword = "󰌋",
+            Snippet = "",
+            Color = "󰏘",
+            File = "󰈙",
+            Reference = "",
+            Folder = "󰉋",
+            EnumMember = "",
+            Constant = "󰏿",
+            Struct = "",
+            Event = "",
+            Operator = "󰆕",
+            TypeParameter = "󰅲",
+        }
+
         cmp.setup({
             snippet = {
                 expand = function(args)
@@ -112,17 +136,29 @@ local cmp = {
                 end
             },
             mapping = {
-                ["<Tab>"] = cmp.mapping(function(fallback)
+                ['<C-Space>'] = cmp.mapping.complete(),
+                ['<CR>'] = cmp.mapping.confirm {
+                    behavior = cmp.ConfirmBehavior.Replace,
+                    select = true,
+                },
+                ['<Tab>'] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_next_item()
                     elseif luasnip.expand_or_jumpable() then
                         luasnip.expand_or_jump()
-                    elseif has_words_before() then
-                        cmp.complete()
                     else
                         fallback()
                     end
-                end, { "i", "s" })
+                end, { 'i', 's' }),
+                ['<S-Tab>'] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { 'i', 's' })
             },
             sources = cmp.config.sources({
                 { name = "nvim_lsp" },
@@ -130,9 +166,24 @@ local cmp = {
                 { name = "buffer" },
                 { name = "path" },
             }),
+            formatting = {
+                format = function(entry, vim_item)
+                -- Kind icons
+                vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+                -- Source
+                vim_item.menu = ({
+                    buffer = "[Buffer]",
+                    nvim_lsp = "[LSP]",
+                    luasnip = "[LuaSnip]",
+                    nvim_lua = "[Lua]",
+                    latex_symbols = "[LaTeX]",
+                })[entry.source.name]
+                return vim_item
+                end
+            },
         })
-    end
-}
+        end
+    }
 
 local mason = {
     "williamboman/mason.nvim",
