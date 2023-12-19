98f51d0e53efe36cb4ebb3c7258c92593254a533
nonumx <nonumx@gmail.com>
2023-12-19 16:17:24 +0800

    新增vim-notify插件

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -5,5 +5,21 @@ return {
   -- 括号匹配
   { "windwp/nvim-autopairs", event = "InsertEnter", opts = {} },
   -- 注释
-  { "numToStr/Comment.nvim", opts = {}, lazy = false }
+  { "numToStr/Comment.nvim", opts = {}, lazy = false },
+  -- 通知窗口
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 3000,
+      max_height = function()
+        return math.floor(vim.o.lines * 0.40)
+      end,
+      max_width = function()
+        return math.floor(vim.o.columns * 0.40)
+      end,
+      on_open = function(win)
+        vim.api.nvim_win_set_config(win, { zindex = 100 })
+      end,
+    },
+  }
 }
