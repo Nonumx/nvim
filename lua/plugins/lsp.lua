9d2be1cca10c0b2f37ff5f26e974f627a769dcfc
nonumx <nonumx@gmail.com>
2023-12-19 11:03:22 +0800

    增加lsp功能

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -21,12 +21,20 @@ return {
         update_in_insert = false,
         severity_sort = true,
       },
-      format = {
-        formatting_options = nil,
-        timeout_ms = nil,
-      }
     },
     config = function(_, opts)
+      local has_cmp, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+      local capabilities = has_cmp and cmp_nvim_lsp.default_capabilities() or {}
+      local has_mlsp, mlsp = pcall(require, "mason-lspconfig")
+      if has_mlsp then
+        -- 通过 mason-lspconfig 自动加载 lsp 并将使用 nvim-cmp
+        -- 作为 lsp 代码提示的引擎
+        mlsp.setup_handlers({
+          function(server_name)
+            require("lspconfig")[server_name].setup({ capabilities = capabilities })
+          end
+        })
+      end
     end
   }
 }
