4d2a291ad371ab329f023a7ed45c0c85dc7083f6
nonumx <nonumx@gmail.com>
2024-04-17 11:32:24 +0800

    :boom: 宇宙重启

--- lua/plugins/nvim-tree.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "nvim-tree/nvim-tree.lua",
-  opts = {},
-  keys = {
-    { "<leader>et", "<cmd>NvimTreeToggle<cr>", desc="Toggle nvim-tree" },
-    { "<leader>ef", "<cmd>NvimTreeFocus<cr>", desc="Focus nvim-tree" },
-  }
-}
