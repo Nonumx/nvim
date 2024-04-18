96a0bf5fb1cf34657d219731e4fff0dcceed3b9b
nonumx <nonumx@gmail.com>
2024-04-18 09:45:09 +0800

    :sparkles: 新增noice配套插件

---
+++ lua/plugins/utils.lua
@@ -0,0 +1,10 @@
+-- 公共插件库
+return {
+    { "nvim-tree/nvim-web-devicons", lazy = true },
+    { "nvim-lua/plenary.nvim", lazy = true },
+    { "MunifTanjim/nui.nvim", lazy = true },
+    -- 通知窗口
+    {
+      "rcarriga/nvim-notify",
+    }
+  }
