c29aed42b200bb3bae1c51dd9016191b28bb2e74
nonumx <nonumx@gmail.com>
2024-12-06 17:30:06 +0800

    增加懒人快捷键

--- init.lua
+++ init.lua
@@ -113,6 +113,12 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+-- 键盘坏了
+vim.keymap.set({ "n" }, "<C-s>", "<cmd>:w<CR>")
+
+-- CV大法
+vim.keymap.set({ "n", "v" }, "<C-c>", "+y")
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
