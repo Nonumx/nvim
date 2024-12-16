59f844f85a7a4a6fc917e22c5e206251fc5c19b2
nonumx <nonumx@gmail.com>
2024-12-16 09:44:38 +0800

    增加vue注释的方法

--- init.lua
+++ init.lua
@@ -822,6 +822,15 @@ require("lazy").setup({
     opts = { signs = false },
   },
 
+  -- 针对vue的注释兼容性解决方案
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    lazy = true,
+    opts = {
+      enable_autocmd = false,
+    },
+  },
+
   { -- Collection of various small independent plugins/modules
     "echasnovski/mini.nvim",
     config = function()
@@ -858,7 +867,13 @@ require("lazy").setup({
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
       local comment = require("mini.comment")
-      comment.setup()
+      comment.setup({
+        options = {
+          custom_commentstring = function()
+            return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
+          end,
+        },
+      })
     end,
   },
   { -- Highlight, edit, and navigate code
