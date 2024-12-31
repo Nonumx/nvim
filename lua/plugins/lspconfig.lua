bf7026f2afa15fa74aaf7e621ae896bbcf94c49f
nonumx <nonumx@gmail.com>
2024-12-31 16:56:36 +0800

    增加telescope插件，修改mason配置

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,6 +2,7 @@ return {
   -- LSP配置
   {
     "neovim/nvim-lspconfig",
+    event = "LazyFile",
     dependencies = {
       -- LSP安装器
       "williamboman/mason.nvim",
@@ -14,10 +15,17 @@ return {
       require("mason-lspconfig").setup_handlers({
         function(server_name)
           -- 整合 capabilities
-          local capabilities = require('blink.cmp').get_lsp_capabilities()
+          local capabilities = require("blink.cmp").get_lsp_capabilities()
           require("lspconfig")[server_name].setup({ capabilities = capabilities })
         end,
       })
     end,
+  },
+
+  -- Mason配置
+  {
+    "williamboman/mason.nvim",
+    -- 直接加载插件，方便在主界面的时候安装lsp等操作
+    opts = {},
   },
 }
