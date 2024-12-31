88c2cb74bcc11516872f1e63bb71218cb5b323bc
nonumx <nonumx@gmail.com>
2024-12-31 16:12:14 +0800

    增加lazydev.nvim方便编写配置

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,25 +1,49 @@
 return {
-  'saghen/blink.cmp',
-  event = "InsertEnter",
-  dependencies = 'rafamadriz/friendly-snippets',
-  version = '*',
+  -- 使用blink.cmp作为补全插件
+  {
+    "saghen/blink.cmp",
+    event = "InsertEnter",
+    dependencies = "rafamadriz/friendly-snippets",
+    version = "*",
 
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    keymap = { preset = 'super-tab' },
-    appearance = {
-      -- sets the fallback highlight groups to nvim-cmp's highlight groups
-      -- useful for when your theme doesn't support blink.cmp
-      -- will be removed in a future release, assuming themes add support
-      use_nvim_cmp_as_default = false,
-      -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- adjusts spacing to ensure icons are aligned
-      nerd_font_variant = "normal",
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      keymap = { preset = "super-tab" },
+      appearance = {
+        -- sets the fallback highlight groups to nvim-cmp's highlight groups
+        -- useful for when your theme doesn't support blink.cmp
+        -- will be removed in a future release, assuming themes add support
+        use_nvim_cmp_as_default = false,
+        -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- adjusts spacing to ensure icons are aligned
+        nerd_font_variant = "normal",
+      },
+      sources = {
+        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+        providers = {
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            -- make lazydev completions top priority (see `:h blink.cmp`)
+            score_offset = 100,
+          },
+        },
+      },
     },
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
+    opts_extend = { "sources.default" },
+  },
+
+  -- 增加nvim配置
+  {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
     },
   },
-  opts_extend = { "sources.default" },
 }
