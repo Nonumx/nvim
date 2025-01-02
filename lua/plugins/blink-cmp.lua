527b7ee9348651ed4f25fdd4c4b4ef1a8a1bf4a1
nonumx <nonumx@gmail.com>
2025-01-02 15:53:15 +0800

    增加enter为补全确认的选项

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -9,7 +9,7 @@ return {
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
-      keymap = { preset = "super-tab" },
+      keymap = { preset = "super-tab", ["<CR>"] = { "accept", "fallback" } },
       appearance = {
         -- sets the fallback highlight groups to nvim-cmp's highlight groups
         -- useful for when your theme doesn't support blink.cmp
