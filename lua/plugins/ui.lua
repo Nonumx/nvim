82a38382c40cd8cb69c44fff989721184c48931d
nonumx <nonumx@gmail.com>
2025-01-02 15:00:52 +0800

    增加界面显示相关的插件

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,15 @@
+return {
+  -- 增强LSP进度状态显示
+  {
+    "j-hui/fidget.nvim",
+    event = "VeryLazy",
+    opts = {},
+  },
+
+  -- Git状态显示
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "LazyFile",
+    opts = {},
+  },
+}
