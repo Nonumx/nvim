ebb351c6b1779cf9c8e7c1b8ecb059661ad9a83d
nonumx <nonumx@gmail.com>
2025-01-03 09:14:38 +0800

    增加终端打开，仅使用终端打开作为插件唤醒的快捷键

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -33,27 +33,19 @@ return {
         enable = false,
       },
     },
-    keys = {
-      {
-        "<leader>ca",
-        "<cmd>Lspsaga code_action<cr>",
-        desc = "[LSP] Code Action",
-      },
-      {
-        "gd",
-        "<cmd>Lspsaga goto_definition<cr>",
-        desc = "[LSP] Goto Definition",
-      },
-      {
-        "<leader>d",
-        "<cmd>Lspsaga hover_doc<cr>",
-        desc = "[LSP] Hover Doc",
-      },
-      {
-        "<F2>",
-        "<cmd>Lspsaga rename<cr>",
-        desc = "[LSP] Rename",
-      },
-    },
+    keys = function()
+      vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<cr>", { desc = "[LSP] Code Action" })
+      vim.keymap.set("n", "gd", "<cmd>Lspsaga goto_definition<cr>", { desc = "[LSP] Goto Definition" })
+      vim.keymap.set("n", "<leader>d", "<cmd>Lspsaga hover_doc<cr>", { desc = "[LSP] Hover Doc" })
+      vim.keymap.set("n", "<F2>", "<cmd>Lspsaga rename<cr>", { desc = "[LSP] Rename" })
+
+      return {
+        {
+          "<leader>t",
+          "<cmd>Lspsaga term_toggle pwsh.exe<cr>",
+          desc = "Open Float Terminal",
+        },
+      }
+    end,
   },
 }
