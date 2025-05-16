185c86b26207b59f838db123532caee709c71081
nonumx <nonumx@gmail.com>
2025-05-16 14:03:53 +0800

    更新配置

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -72,6 +72,21 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
-    opts = {},
+    opts = {
+      options = {
+        component_separators = "",
+        section_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+        lualine_b = { "branch", "filename" },
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = { "filetype", "progress" },
+        lualine_z = {
+          { "location", separator = { right = "" }, left_padding = 2 },
+        },
+      },
+    },
   },
 }
