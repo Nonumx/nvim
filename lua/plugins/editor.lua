377f112f12b51eace9f42c69440dc94603eb78be
nonumx <nonumx@gmail.com>
2025-05-16 10:31:13 +0800

    更新配置

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -14,4 +14,56 @@ return {
       },
     },
   },
+  -- LSP状态和其他进度
+  {
+    "j-hui/fidget.nvim",
+    event = "VeryLazy",
+    opts = {},
+  },
+  -- 诊断、引用、修复等等列表
+  {
+    "folke/trouble.nvim",
+    opts = {},
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Trouble: Diagnostics",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Trouble: Buffer Diagnostics",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Trouble: Symbols",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "Trouble: LSP Definitions / references / ...",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Trouble: Location List",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Trouble: Quickfix List",
+      },
+    },
+  },
+  -- Git状态
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "VeryLazy",
+    opts = {
+      trouble = true,
+    },
+  },
 }
