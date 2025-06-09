cbbe43f593881e1f980ca068f0411a897f2998c8
nonumx <nonumx@gmail.com>
2025-06-09 23:18:56 +0800

    LSP配置改为从配置文件夹获取名称

--- init.lua
+++ init.lua
@@ -151,7 +151,12 @@ end)
 
 -- [[ LSP配置 ]]
 later(function()
-  vim.lsp.enable("luals")
+  local lsp_dir = vim.fn.stdpath("config") .. "/lsp/"
+  local lsp_configs = vim.fn.globpath(lsp_dir, "*.lua", false, true)
+  for _, lsp_config in ipairs(lsp_configs) do
+    local lsp_name = vim.fn.fnamemodify(lsp_config, ":t:r")
+    vim.lsp.enable(lsp_name)
+  end
 end)
 
 -- [[ 代码格式化 ]]
