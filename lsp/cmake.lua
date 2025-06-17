db3c24bae5edaf11bd9c2e59e0f77dab0ed6e5fc
nonumx <nonumx@gmail.com>
2025-06-17 21:06:43 +0800

    更新配置

--- lsp/cmake.lua
+++ lsp/cmake.lua
@@ -1,3 +1,13 @@
+local M = {}
+
+M.vswhere_path = vim.fn.stdpath("data") .. "/tools/vswhere.exe"
+
+M.warn_vswhere_on_first = true
+
+M.warn_vspath_on_first = true
+
+local function auto_reconfigure() end
+
 return {
   cmd = { "cmake-language-server" },
   filetypes = { "cmake" },
@@ -5,4 +15,27 @@ return {
   init_options = {
     buildDirectory = "build",
   },
+  on_attach = function()
+    if not vim.loop.fs_stat(M.vswhere_path) then
+      if M.warn_vswhere_on_first then
+        local message = {
+          "Failed to locate `vswhere.exe`",
+          "Please copy `vswhere.exe` to following path:",
+          "```lua",
+          'vim.fn.stdpath("data") .. "/tools/"',
+          "```",
+        }
+        vim.notify(table.concat(message, "\n"), "warn", { title = "lsp/cmake" })
+        M.warn_vswhere_on_first = false
+      end
+      return
+    end
+    local cmake_augroup = vim.api.nvim_create_augroup("CMakeAugroup", { clear = true })
+    vim.api.nvim_create_autocmd("BufWritePost", {
+      group = cmake_augroup,
+      pattern = { "CMakeLists.txt", "*.cmake" },
+      callback = auto_reconfigure,
+    })
+    auto_reconfigure()
+  end,
 }
