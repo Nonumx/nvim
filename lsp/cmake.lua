d465bc2f6b8703ca8750c7a4ee62f015549fddcd
nonumx <nonumx@gmail.com>
2025-06-17 22:15:55 +0800

    更新配置

--- lsp/cmake.lua
+++ lsp/cmake.lua
@@ -6,7 +6,60 @@ M.warn_vswhere_on_first = true
 
 M.warn_vspath_on_first = true
 
-local function auto_reconfigure() end
+M.name = "lsp/cmake"
+
+M.auto_reconfigure = function()
+  local Job = require("plenary.job")
+  ---@diagnostic disable-next-line:missing-fields
+  local res = Job:new({
+    command = M.vswhere_path,
+    args = {
+      "-property",
+      "installationPath",
+    },
+  }):sync()
+  if res == nil then
+    if M.warn_vspath_on_first then
+      local message = {
+        "Failed to locate `Visual Studio`",
+        "Please verify you have VS2017 or above",
+        "installed on your machine",
+      }
+      vim.notify(table.concat(message, "\n"), "warn", { title = "lsp/cmake" })
+      M.warn_vspath_on_first = false
+    end
+    return
+  end
+  local vspath = res[1]
+  local vcvars_path = vspath .. [[\VC\Auxiliary\Build\vcvarsall.bat]]
+  local command_args = {
+    "%VCVars% x64",
+    "powershell.exe -c cmake.exe -B build -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE",
+  }
+  ---@diagnostic disable-next-line:missing-fields
+  Job:new({
+    command = "cmd.exe",
+    args = {
+      "/c",
+      table.concat(command_args, " && "),
+    },
+    env = {
+      VCVars = string.format('"%s"', vcvars_path),
+    },
+    on_exit = function(j, return_val)
+      -- on_exit在异步上下文中
+      -- vim.notify必须在安全上下文中执行
+      vim.schedule(function()
+        local j_message = table.concat(j:result() or {}, "\n")
+        if return_val == 0 then
+          vim.notify(j_message, "info", { title = M.name })
+        else
+          vim.notify(j_message, "warn", { title = M.name })
+        end
+      end)
+    end,
+  }):start()
+end
 
 return {
   cmd = { "cmake-language-server" },
@@ -25,7 +78,7 @@ return {
           'vim.fn.stdpath("data") .. "/tools/"',
           "```",
         }
-        vim.notify(table.concat(message, "\n"), "warn", { title = "lsp/cmake" })
+        vim.notify(table.concat(message, "\n"), "warn", { title = M.name })
         M.warn_vswhere_on_first = false
       end
       return
@@ -34,8 +87,7 @@ return {
     vim.api.nvim_create_autocmd("BufWritePost", {
       group = cmake_augroup,
       pattern = { "CMakeLists.txt", "*.cmake" },
-      callback = auto_reconfigure,
+      callback = M.auto_reconfigure,
     })
-    auto_reconfigure()
   end,
 }
