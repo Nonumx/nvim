f326c31320ad7951607fa6cc227645cfcc6914f4
nonumx <nonumx@gmail.com>
2025-07-09 10:35:07 +0800

    更新配置

--- init.lua
+++ init.lua
@@ -353,3 +353,12 @@ later(function()
     },
   })
 end)
+
+-- [[ 顶部导航栏 ]]
+later(function()
+  add({ source = "Bekaboo/dropbar.nvim" })
+  local dropbar_api = require("dropbar.api")
+  vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
+  vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
+  vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
+end)
