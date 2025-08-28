8397dcccdd642297f55d557656c9072e968549be
nonumx <nonumx@gmail.com>
2025-08-28 14:45:23 +0800

    更新配置

--- lua/plugins/colorschema.lua
+++ lua/plugins/colorschema.lua
@@ -1,5 +1,21 @@
 local add = MiniDeps.add
 
 add({ source = "everviolet/nvim", name = "evergarden" })
-require("evergarden").setup()
-
+require("evergarden").setup({
+  theme = {
+    variant = "fall", -- 'winter'|'fall'|'spring'|'summer'
+    accent = "green",
+  },
+  editor = {
+    transparent_background = false,
+    sign = { color = "none" },
+    float = {
+      color = "mantle",
+      solid_border = false,
+    },
+    completion = {
+      color = "surface0",
+    },
+  },
+})
+vim.cmd("colorscheme evergarden-fall")
