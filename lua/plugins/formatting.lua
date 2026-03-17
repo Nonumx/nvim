2ad2a26b4458ea5aabc39dc03ea6a580902c9beb
nonumx <nonumx@gmail.com>
2026-03-17 11:09:32 +0800

    更新配置

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -4,9 +4,6 @@ later(function()
   add({ source = "stevearc/conform.nvim" })
   require("conform").setup({
     notify_on_error = false,
-    format_on_save = {
-      timeout_ms = 500,
-    },
     formatters_by_ft = {
       lua = { "stylua" },
       cpp = { "clang-format" },
