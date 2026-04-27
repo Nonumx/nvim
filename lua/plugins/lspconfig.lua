ef7cc1e184c38c0cacd80e164d08eddb939e1c6d
nonumx <nonumx@gmail.com>
2026-04-27 21:54:50 +0800

    更新配置

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -66,6 +66,36 @@ return {
         vim.lsp.config(name, config)
         vim.lsp.enable(name)
       end
+
+      -- Snacks picker LSP keymaps
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspKeymaps", { clear = true }),
+        callback = function(args)
+          local bufnr = args.buf
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if not client then
+            return
+          end
+
+          if client.server_capabilities.definitionProvider then
+            vim.keymap.set("n", "gd", function()
+              Snacks.picker.lsp_definitions()
+            end, { buffer = bufnr, desc = "Goto Definition", silent = true })
+          end
+
+          if client.server_capabilities.referencesProvider then
+            vim.keymap.set("n", "gr", function()
+              Snacks.picker.lsp_references()
+            end, { buffer = bufnr, desc = "References", silent = true, nowait = true })
+          end
+
+          if client.server_capabilities.documentSymbolProvider then
+            vim.keymap.set("n", "<leader>s", function()
+              Snacks.picker.lsp_symbols()
+            end, { buffer = bufnr, desc = "LSP Symbols", silent = true })
+          end
+        end,
+      })
     end,
   },
 }
