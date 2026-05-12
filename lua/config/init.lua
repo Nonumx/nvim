0616a18071f0b0a3de1100c21d76da430e8a424c
nonumx <nonumx@gmail.com>
2026-05-12 09:57:54 +0800

    更新配置

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -2,6 +2,20 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- rtp 优化
+vim.g.loaded_gzip = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_zip = 1
+vim.g.loaded_zipPlugin = 1
+vim.g.loaded_getscript = 1
+vim.g.loaded_getscriptPlugin = 1
+vim.g.loaded_vimball = 1
+vim.g.loaded_vimballPlugin = 1
+vim.g.loaded_2html_plugin = 1
+vim.g.loaded_rrhelper = 1
+vim.g.loaded_netrwPlugin = 1
+
 -- options
 local o = vim.opt
 
@@ -12,6 +26,7 @@ o.expandtab = true
 
 -- 状态栏
 o.laststatus = 3 -- 全局状态栏
+o.showmode = false -- 不进行状态模式回显
 o.showcmd = false -- 不进行快捷键回显
 
 -- keymaps
